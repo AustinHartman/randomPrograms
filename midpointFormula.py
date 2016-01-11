f57d4880857289e86fd77b0382175eb552456e67
Austin Hartman <hartmana99@gmail.com>
2016-01-10 19:48:50 -0800

    Added rerun

--- midpointFormula.py
+++ midpointFormula.py
@@ -4,4 +4,9 @@ def midpointFormula():
     pt2 = [int(input("What is the 2nd x coordinate?\n")), int(input("What is the 2nd y coordinate?\n"))]
     midpoint = [(pt1[0] + pt2[0]) / 2, (pt1[1] + pt2[1]) / 2]
     print(midpoint)
+    rerun = input("Would you like to run it again? y or n\n")
+    if rerun == "y":
+        midpointFormula()
+    else:
+        exit(0)
 midpointFormula()
