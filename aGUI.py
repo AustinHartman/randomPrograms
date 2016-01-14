143ad4e4965f4a206ad0117cafe6043328a78e37
Austin Hartman <hartmana99@gmail.com>
2016-01-13 21:27:01 -0800

    Added header and added function to be used later

--- aGUI.py
+++ aGUI.py
@@ -1,10 +1,21 @@
-import tkinter
-window = tkinter.Tk()
+from tkinter import *
+def decimalToBinary(x):
+
+   if x > 1:
+       decimalToBinary(x//2)
+   print(x % 2, end = '')
+
+#number = int(input("What is your number?\n"))
+window = Tk()
 window.title("Boring GUI")
 window.geometry("480x480")
-button1 = tkinter.Button(window, text="Button 1")
-button2 = tkinter.Button(window, text="Button 2")
-button1.pack()
-button2.pack()
+header = Label(window, text="This might become a basic calculator!")
+quadForm = Button(window, text="Quadratic Formula")
+binConv = Button(window, text="Binary Converter")
+quit = Button(window, text="Quit", command=window.quit)
+header.pack()
+quadForm.pack()
+binConv.pack()
+quit.pack()
 window.mainloop()
 
