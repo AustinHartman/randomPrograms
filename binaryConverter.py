e7444befe8831c2e37f265a4fcc9183aaa412c05
Austin Hartman <hartmana99@gmail.com>
2016-01-19 19:19:37 -0800

    Also added rerun via callback

--- binaryConverter.py
+++ binaryConverter.py
@@ -1,9 +1,20 @@
 #Convert decimal to binary with a recursive function
 def decimalToBinary(x):
 
-   if x > 1:
-       decimalToBinary(x//2)
-   print(x % 2, end = '')
+    if x > 1:
+        decimalToBinary(x//2)
+    print(x % 2, end = '')
+
+
+def callback():
+    repeat = input("\nRun again?\n")
+    if repeat == 'yes':
+        number = int(input("What is your number?\n"))
+        decimalToBinary(number)
+    else:
+        quit(0)
 
 number = int(input("What is your number?\n"))
 decimalToBinary(number)
+callback()
+
